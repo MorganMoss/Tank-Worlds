11ebb2d6e5f139b11030dca52554997f6be1a7c1
mdewet021 <mdewet021@student.wethinkcode.co.za>
2022-04-08 13:51:17 +0200

    fixed typo

--- src/main/java/za/co/wethinkcode/robotworlds/server/Client.java
+++ src/main/java/za/co/wethinkcode/robotworlds/server/Client.java
@@ -6,7 +6,7 @@ public class Client {
     public final String robotName;
     public final Socket socket;
     public Request currentRequest;
-    public response currentResponse;
+    public Response currentResponse;
 
     giveRequest(String input) {}
 
