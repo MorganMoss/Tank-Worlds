903de547ae15d718ab2c8863777777139384bc86
MfundoVala <mfundosapio@gmail.com>
2022-04-13 13:20:31 +0200

    moved visual gui files and implemented serialize in gui

---
+++ src/main/java/za/co/wethinkcode/robotworlds/client/SwingUI/Enemy.java
@@ -0,0 +1,32 @@
+package za.co.wethinkcode.robotworlds.client.SwingUI;
+
+import java.awt.*;
+
+public class Enemy extends Thread{
+    private String name = "Enemy1";
+    public String getTankName(){return this.name;}
+
+    private String sprite = "etank";
+    public String getSpriteName(){return this.sprite;}
+
+    private String deadImage = "deademoji.png";
+    public String getDeadImage(){return this.deadImage;}
+
+    private int enemyHealth = 35;
+    public int getTankHealth(){return this.enemyHealth;}
+    public void setTankHealth(int health){this.enemyHealth=health;}
+
+    private Direction enemyTankDirection = Direction.Up;
+    public Direction getTankDirection(){return this.enemyTankDirection;}
+    public void setTankDirection(Direction direction){this.enemyTankDirection =direction;}
+    public void takeHit(int hit){this.enemyHealth-=hit;}
+
+
+    @Override
+    public void run(){
+
+    }
+    private void enemySpawn(Graphics g){
+
+    }
+}
---
+++ src/main/java/za/co/wethinkcode/robotworlds/client/SwingUI/HelperMethods.java
@@ -0,0 +1,91 @@
+package za.co.wethinkcode.robotworlds.client.SwingUI;//import javafx.scene.media.MediaPlayer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.google.gson.Gson;
+
+
+class HelperMethods {
+    private static Random RANDOM = new Random();
+
+    static boolean nextBoolean() {
+        return RANDOM.nextBoolean();
+    }
+
+    /**
+     * generate a random number less than giving number
+     * @param endExclusive  exclusive maximum number
+     */
+    static int nextInt(final int endExclusive) {
+        return RANDOM.nextInt(endExclusive);
+    }
+
+    /**
+     * Play an audio file located under directory "assets/audios"
+     */
+//    static void playAudio(final String audioFile) {
+//        File file = new File("assets/audios/" + audioFile);
+//        Media media = new Media(file.toURI().toString());
+//        new MediaPlayer(media).play();
+//    }
+
+    /**
+     * Get {@link Image} from given file
+     * @param imageFileName name of image file under directory "assets/images"
+     */
+    static Image getImage(final String imageFileName) {
+        return new ImageIcon("assets/images/" + imageFileName).getImage();
+    }
+
+    static void sleepSilently(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            // -> Ignore
+        }
+    }
+
+    /**
+     * Set Swing Theme: Windows or Nimbus
+     */
+    static void setTheme() {
+        String theme = System.getProperty("os.name").startsWith("Windows") ? "Windows" : "Nimbus";
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if (theme.equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(HelperMethods.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    static String serialize(Object object){
+        Gson gson = new Gson();
+        return gson.toJson(object);
+    }
+
+
+
+// TANK DIAGONAL
+    //        int dist = (WIDTH - 120) / 11;
+//        for (int i = 0; i < 12; i++) {
+//            client.Direction direction = client.Direction.values()[i % 8];
+//            g.drawImage(direction.getImage("etank"), 42 + dist * i, HEIGHT / 2 + 100, null);
+//        }
+
+    //MISSLE DIAGONAL
+
+    //        boolean upBull = g.drawImage(client.Direction.Left.getImage("missile"), WIDTH / 2 - 18, bulletY, null);
+//        g.drawImage(client.Direction.Right.getImage("missile"), WIDTH / 2 + 14, bulletY, null);
+//        g.drawImage(client.Direction.Down.getImage("missile"), WIDTH / 2, bulletY + 12, null);
+//        g.drawImage(client.Direction.LeftUp.getImage("missile"), WIDTH / 2 - 12, bulletY - 18, null);
+//        g.drawImage(client.Direction.RightUp.getImage("missile"), WIDTH / 2 + 12, bulletY - 18, null);
+//        g.drawImage(client.Direction.RightDown.getImage("missile"), WIDTH / 2 + 12, bulletY + 12, null);
+//        g.drawImage(client.Direction.LeftDown.getImage("missile"), WIDTH / 2 - 12, bulletY + 12, null);
+}
