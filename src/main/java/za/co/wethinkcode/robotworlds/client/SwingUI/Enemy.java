8dd586d3ec9d13506f0f07385a9c77ccda8d4aa8
MfundoVala <mfundosapio@gmail.com>
2022-04-13 13:31:25 +0200

    pushed swing ui

--- src/main/java/za/co/wethinkcode/robotworlds/client/SwingUI/Enemy.java
+++ src/main/java/za/co/wethinkcode/robotworlds/client/SwingUI/Enemy.java
@@ -2,24 +2,24 @@ package za.co.wethinkcode.robotworlds.client.SwingUI;
 
 import java.awt.*;
 
-public class Enemy extends Thread{
+public class Enemy extends Thread {
+
     private String name = "Enemy1";
-    public String getTankName(){return this.name;}
-
     private String sprite = "etank";
-    public String getSpriteName(){return this.sprite;}
-
     private String deadImage = "deademoji.png";
+    private Direction direction = Direction.Up;
+    private int health = 35;
+
+
+    public String getTankName(){return this.name;}
+    public String getSpriteName(){return this.sprite;}
     public String getDeadImage(){return this.deadImage;}
 
-    private int enemyHealth = 35;
-    public int getTankHealth(){return this.enemyHealth;}
-    public void setTankHealth(int health){this.enemyHealth=health;}
-
-    private Direction enemyTankDirection = Direction.Up;
-    public Direction getTankDirection(){return this.enemyTankDirection;}
-    public void setTankDirection(Direction direction){this.enemyTankDirection =direction;}
-    public void takeHit(int hit){this.enemyHealth-=hit;}
+    public int getTankHealth(){return this.health;}
+    public void setTankHealth(int health){this.health =health;}
+    public Direction getTankDirection(){return this.direction;}
+    public void setTankDirection(Direction direction){this.direction =direction;}
+    public void takeHit(int hit){this.health -=hit;}
 
 
     @Override
