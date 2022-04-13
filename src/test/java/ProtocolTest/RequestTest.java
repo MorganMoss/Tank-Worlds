772917a8f69fa80fe6f4a23a09782e85dd0c033d
sfikizol021 <sfikizol021@student.wethinkcode.co.za>
2022-04-13 14:27:30 +0200

    test for serialization

--- src/test/java/ProtocolTest/RequestTest.java
+++ src/test/java/ProtocolTest/RequestTest.java
@@ -1,4 +1,27 @@
 package ProtocolTest;
 
-public class RequestTest {
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.robotworlds.protocol.Request;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RequestTest{
+ @Test
+ public void testSerializationForward() {
+  Request request = new Request("sisipho","forward");
+  assertEquals("{\"clientName\":\"sisipho\",\"command\":\"forward\"}",request.serialize());
+
+ }
+ @Test
+ public void testSerializationFire() {
+  Request request = new Request("maggie","fire");
+  assertEquals("{\"clientName\":\"maggie\",\"command\":\"fire\"}",request.serialize());
+
+ }
+ @Test
+ public void testSerializationLook() {
+  Request request = new Request("morgan","look");
+  assertEquals("{\"clientName\":\"morgan\",\"command\":\"look\"}",request.serialize());
+
+ }
 }
