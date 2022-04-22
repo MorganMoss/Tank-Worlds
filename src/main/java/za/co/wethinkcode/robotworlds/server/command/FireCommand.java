11f14570f244a1a99d521980091f52846b868917
sfikizol021 <sfikizol021@student.wethinkcode.co.za>
2022-04-22 11:43:15 +0200

    working on fire command

--- src/main/java/za/co/wethinkcode/robotworlds/server/command/FireCommand.java
+++ src/main/java/za/co/wethinkcode/robotworlds/server/command/FireCommand.java
@@ -1,14 +1,42 @@
 package za.co.wethinkcode.robotworlds.server.command;
 
+import za.co.wethinkcode.robotworlds.server.Position;
 import za.co.wethinkcode.robotworlds.server.World;
+import za.co.wethinkcode.robotworlds.server.robot.Robot;
 
-public class FireCommand extends Command{
+public class FireCommand extends Command {
     public FireCommand(String robotName) {
         super(robotName);
     }
 
     @Override
-    public void execute(World world) {
+    public String execute(World world) {
         // TODO : execute command
+//        int distance = 5;
+//        Robot robot = world.getRobot(robotName);
+//        world.get
+//
+//
+//        Position initialBullet = robot.getPosition();
+//        Position finalBullet = new Position(0,0);//add distance depending on the direction.
+//
+//        if (robot.getDirection().getAngle() == 0) {
+//            finalBullet = new Position(initialBullet.getX(), initialBullet.getY() + distance);
+//        } else if (robot.getDirection().getAngle() == 90) {
+//            finalBullet = new Position(initialBullet.getX() + distance, initialBullet.getY());
+//
+//        } else if (robot.getDirection().getAngle() == 180) {
+//            finalBullet = new Position(initialBullet.getX(), initialBullet.getY() - distance);
+//
+//        } else if (robot.getDirection().getAngle() == 270) {
+//            finalBullet = new Position(initialBullet.getX() - distance, initialBullet.getY());
+//        }
+//
+//        if (!world.pathBlocked(initialBullet, finalBullet)) {
+//            return "miss";
+//        } else {
+//
+//        }
+    return null;
     }
 }
