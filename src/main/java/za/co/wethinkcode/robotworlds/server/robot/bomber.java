b47255619d29f4b6a1b939723d3b3fc0d076c60d
wtc <sfikizol021@student.wethinkcode.co.za>
2022-05-05 14:38:10 +0200

    added some robots

---
+++ src/main/java/za/co/wethinkcode/robotworlds/server/robot/bomber.java
@@ -0,0 +1,7 @@
+package za.co.wethinkcode.robotworlds.server.robot;
+
+public class bomber extends Robot{
+    public bomber(String name) {
+        super(name, 25, 2, 5, 3, 1);
+    }
+}
---
+++ src/main/java/za/co/wethinkcode/robotworlds/server/robot/machine.java
@@ -0,0 +1,7 @@
+package za.co.wethinkcode.robotworlds.server.robot;
+
+public class machine extends Robot{
+    public machine(String name) {
+        super(name, 25, 1, 2, 3, 4);
+    }
+}
