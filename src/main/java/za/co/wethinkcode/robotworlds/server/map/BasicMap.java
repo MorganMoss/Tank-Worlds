ab58a28104549e5a78981e6ed4881ad9bf73c688
mdewet021 <mdewet021@student.wethinkcode.co.za>
2022-05-09 13:53:47 +0200

    changed basicmap to have 1 obstacle

--- src/main/java/za/co/wethinkcode/robotworlds/server/map/BasicMap.java
+++ src/main/java/za/co/wethinkcode/robotworlds/server/map/BasicMap.java
@@ -1,12 +1,11 @@
 package za.co.wethinkcode.robotworlds.server.map;
 
+import za.co.wethinkcode.robotworlds.server.Position;
+import za.co.wethinkcode.robotworlds.server.obstacle.Obstacle;
+import za.co.wethinkcode.robotworlds.server.obstacle.SquareObstacle;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
-import za.co.wethinkcode.robotworlds.server.obstacle.Obstacle;
-import za.co.wethinkcode.robotworlds.server.Position;
-import za.co.wethinkcode.robotworlds.server.obstacle.SquareObstacle;
 
 public class BasicMap implements Map{
     private final Position size;
@@ -17,21 +16,7 @@ public class BasicMap implements Map{
 
     public List<Obstacle> getObstacles() {
         List<Obstacle> obstacleList = new ArrayList<>();
-        for(int x=-300;x<300;x+=25){
-            Obstacle xBrick = new SquareObstacle(25,new Position(x,300));
-            obstacleList.add(xBrick);
-        }
-        for(int y=-200;y<300;y+=25){
-            Obstacle yBrick = new SquareObstacle(25, new Position(0,y));
-            obstacleList.add(yBrick);
-        }
-        Obstacle worldBrick = new SquareObstacle(25, new Position(-200,100));
-        Obstacle worldBrick2 = new SquareObstacle(25, new Position(150,-100));
-        obstacleList.add(worldBrick);
-        obstacleList.add(worldBrick2);
-
         obstacleList.add(new SquareObstacle(3, new Position(0,0)));
-
         return obstacleList;
     }
     public Position getMapSize() {
