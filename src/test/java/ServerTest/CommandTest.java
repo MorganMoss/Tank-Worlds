8d36815004ba046026851d4d7cba362e26d23cf2
wtc <sfikizol021@student.wethinkcode.co.za>
2022-05-09 16:39:33 +0200

    some test

--- src/test/java/ServerTest/CommandTest.java
+++ src/test/java/ServerTest/CommandTest.java
@@ -1,14 +1,90 @@
 package ServerTest;
 
 import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.robotworlds.server.Position;
+import za.co.wethinkcode.robotworlds.server.World;
+import za.co.wethinkcode.robotworlds.server.command.*;
+import za.co.wethinkcode.robotworlds.server.map.BasicMap;
+import za.co.wethinkcode.robotworlds.server.robot.BasicRobot;
+import za.co.wethinkcode.robotworlds.server.robot.Robot;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandTest {
     @Test
-    public void testBackCommand(){
-
-
+    public void testRepairCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "hal");
+        world.add(robot);
+        RepairCommand repairCommand = new RepairCommand(robot.getRobotName());
+        assertEquals("Repair in progress",repairCommand.execute(world));
     }
+    @Test
+    public void testLaunch(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "halk");
+        world.add(robot);
+        LaunchCommand launch = new LaunchCommand(robot.getRobotName(),"BasicRobot");
+        assertEquals("Success",launch.execute(world));
+    }
+    @Test
+    public void testIdle(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "halk");
+        world.add(robot);
+        IdleCommand idleCommand = new IdleCommand(robot.getRobotName());
+        assertEquals("idle",idleCommand.execute(world));
+    }
+    @Test
+    public void testReloadCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "halk");
+        world.add(robot);
+        ReloadCommand reloadCommand = new ReloadCommand(robot.getRobotName());
+        assertEquals("Reload in progress",reloadCommand.execute(world));
+    }
+    @Test
+    public void testFireCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "Peter");
+        world.add(robot);
+        FireCommand fireCommand = new FireCommand(robot.getRobotName());
+        assertEquals("Success",fireCommand.execute(world));
+    }
+
+    @Test
+    public void testLeftCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "Roger");
+        LeftCommand leftCommand = new LeftCommand(robot.getRobotName());
+        world.add(robot);
+        assertEquals("Success",leftCommand.execute(world));
+    }
+    @Test
+    public void testRightCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "Larry");
+        RightCommand rightCommand = new RightCommand(robot.getRobotName());
+        world.add(robot);
+        assertEquals("Success",rightCommand.execute(world));
+    }
+    @Test
+    public void testBackCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "Moss");
+        world.add(robot);
+        Command backCommand = new BackCommand(robot.getRobotName(),"5");
+        assertEquals("Success",backCommand.execute(world));
+    }
+    @Test
+    public void testForwardCommand(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Robot robot = new BasicRobot(world, "Moss");
+        ForwardCommand forwardCommand = new ForwardCommand(robot.getRobotName(),"5");
+        world.add(robot);
+        assertEquals("Success",forwardCommand.execute(world));
+    }
+
+
 }
--- src/test/java/ServerTest/RobotTest.java
+++ src/test/java/ServerTest/RobotTest.java
@@ -5,6 +5,7 @@ import za.co.wethinkcode.robotworlds.server.Position;
 import za.co.wethinkcode.robotworlds.server.World;
 import za.co.wethinkcode.robotworlds.server.map.BasicMap;
 import za.co.wethinkcode.robotworlds.server.robot.BasicRobot;
+import za.co.wethinkcode.robotworlds.server.robot.Sniper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +22,22 @@ public class RobotTest {
         assertEquals(0,basicRobot.getKills());
         assertEquals(10,basicRobot.getFiringDistance());
         assertEquals(10,basicRobot.getVisibilityDistance());
+
+
+
+    }
+    @Test
+    public void testSniper(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Sniper sniper = new Sniper(world, "Hal");
+        assertEquals("Hal",sniper.getRobotName());
+        assertEquals(5,sniper.getRange());
+        assertEquals(1,sniper.getCurrentAmmo());
+        assertEquals(3,sniper.getCurrentShield());
+        assertEquals(0,sniper.getDeaths());
+        assertEquals(0,sniper.getKills());
+        assertEquals(5,sniper.getFiringDistance());
+        assertEquals(12,sniper.getVisibilityDistance());
 
 
 
