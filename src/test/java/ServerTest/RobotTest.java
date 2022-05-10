4090daa0436391757dd34912a5140f4686a7a07c
wtc <sfikizol021@student.wethinkcode.co.za>
2022-05-10 09:20:41 +0200

    robot test

--- src/test/java/ServerTest/RobotTest.java
+++ src/test/java/ServerTest/RobotTest.java
@@ -5,6 +5,8 @@ import za.co.wethinkcode.robotworlds.server.Position;
 import za.co.wethinkcode.robotworlds.server.World;
 import za.co.wethinkcode.robotworlds.server.map.BasicMap;
 import za.co.wethinkcode.robotworlds.server.robot.BasicRobot;
+import za.co.wethinkcode.robotworlds.server.robot.Bomber;
+import za.co.wethinkcode.robotworlds.server.robot.Machine;
 import za.co.wethinkcode.robotworlds.server.robot.Sniper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,7 +39,39 @@ public class RobotTest {
         assertEquals(0,sniper.getDeaths());
         assertEquals(0,sniper.getKills());
         assertEquals(5,sniper.getFiringDistance());
-        assertEquals(12,sniper.getVisibilityDistance());
+        assertEquals(11,sniper.getVisibilityDistance());
+
+
+
+    }
+    @Test
+    public void testMachine(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Machine machine = new Machine(world, "Hal");
+        assertEquals("Hal",machine.getRobotName());
+        assertEquals(5,machine.getRange());
+        assertEquals(2,machine.getCurrentAmmo());
+        assertEquals(1,machine.getCurrentShield());
+        assertEquals(0,machine.getDeaths());
+        assertEquals(0,machine.getKills());
+        assertEquals(4,machine.getFiringDistance());
+        assertEquals(9,machine.getVisibilityDistance());
+
+
+
+    }
+    @Test
+    public void testBomber(){
+        World world = new World(new BasicMap(new Position(100,100)));
+        Bomber bomber = new Bomber(world, "Hal");
+        assertEquals("Hal",bomber.getRobotName());
+        assertEquals(5,bomber.getRange());
+        assertEquals(5,bomber.getCurrentAmmo());
+        assertEquals(2,bomber.getCurrentShield());
+        assertEquals(0,bomber.getDeaths());
+        assertEquals(0,bomber.getKills());
+        assertEquals(1,bomber.getFiringDistance());
+        assertEquals(9,bomber.getVisibilityDistance());
 
 
 
