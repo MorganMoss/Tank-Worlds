f6b528d8457d0910ac38720bbf77446cb349c26a
MorganMoss <raveressmoss@gmail.com>
2022-05-12 13:31:55 +0200

    Firing working sometimes, but sometimes misses a robot completey

--- src/main/java/za/co/wethinkcode/robotworlds/server/command/FireCommand.java
+++ src/main/java/za/co/wethinkcode/robotworlds/server/command/FireCommand.java
@@ -6,6 +6,9 @@ import za.co.wethinkcode.robotworlds.shared.Robot;
 
 import java.util.List;
 
+import static java.lang.Math.*;
+import static java.lang.Math.toRadians;
+
 public class FireCommand extends Command{
 
     public FireCommand(String robotName) {
@@ -21,11 +24,19 @@ public class FireCommand extends Command{
             if (robot.getCurrentAmmo() > 0) {
                 robot.decreaseAmmo();
 
-                List<Position> bulletList = World.getBulletList(robot);
+//                List<Position> bulletList = World.getBulletList(robot);
+//
+//                Position finalBullet = bulletList.get(bulletList.size() - 1);
+//
 
-                Position finalBullet = bulletList.get(bulletList.size() - 1);
+                Position bulletDestination =  new Position(
+                        (int) (robot.getPosition().getX()
+                                + round(robot.getFiringDistance() * sin(toRadians(robot.getDirection().getAngle())))),
+                        (int) (robot.getPosition().getY()
+                                + round(robot.getFiringDistance() * cos(toRadians(robot.getDirection().getAngle()))))
+                );
 
-                String result = World.pathBlocked(robot, finalBullet);
+                String result = World.pathBlocked(robot, bulletDestination);
 
                 if (result.contains("hit enemy ")){
                         Robot enemy = World.getRobot(result.replace("hit enemy ", ""));
